6174ce8787a527a69c2f290356f125d5336cc544
hnsCreations <harin.ncsl@gmail.com>
2017-10-03 19:17:59 +0530

    3  10

---
+++ dashboard/customer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Customer Registration</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="form.css">
+</head>
+<body>
+	<!-- from here -->
+	<div class="window_container">
+		<div class="header">
+			<span><u>New Customer</u></span>						
+		</div>
+
+		<form method="post" action="customer.php">
+			<table  class="form_table ">
+				<tr class="form_table_row">
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+					<td class="form_table_row_description">National Identity Card Number</td>
+				</tr>									
+			</table>
+		</form>
+	</div>
+</body>
+</html>
