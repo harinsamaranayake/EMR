369d7dba6968ca7dc8f1621fa5f388507552cce1
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2017-11-08 19:06:00 +0530

    Update MainActivity.java

--- members/Pamud/MainActivity.java
+++ members/Pamud/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.samud.emrapp;
+package com.example.samud.emrapplication;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -17,29 +17,48 @@ public class MainActivity extends AppCompatActivity {
     private int counter=5;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name=(EditText)findViewById(R.id.etName);
+       Name=(EditText)findViewById(R.id.etName);
         Password=(EditText)findViewById(R.id.etPassword);
         info=(TextView) findViewById(R.id.tvinfo);
         Login=(Button) findViewById(R.id.btnLogin);
 
         info.setText("No of attempts remaining:5");
 
-        Login.setOnClickListener(new View.OnClickListener() {
+       /* Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validate(Name.getText().toString(),Password.getText().toString());
             }
-        });
+        });*/
+
 
 
     }
 
-    private void validate(String userName,String userPassword){
+    public void OnLogin(View view){
+
+      String username=Name.getText().toString();
+        String password= Password.getText().toString();
+        String type="login";
+        BackgroundWoker backgroundWoker=new BackgroundWoker(this);
+        backgroundWoker.execute(type,username,password);
+
+
+    }
+
+
+
+
+
+
+
+   /* private void validate(String userName,String userPassword){
 
         if ((userName.equals("Admin"))&&(userPassword.equals("1234"))){
             Intent intent= new Intent(MainActivity.this,Main2Activity.class);
@@ -53,5 +72,5 @@ public class MainActivity extends AppCompatActivity {
                 Login.setEnabled(false);
             }
         }
-    }
+    }*/
 }
