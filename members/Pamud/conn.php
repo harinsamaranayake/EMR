38b1aeece7d7feb846b1c58dbd2ccc644cf59f7e
Pamud624 <31804008+Pamud624@users.noreply.github.com>
2017-11-08 19:21:52 +0530

    Add files via upload

---
+++ members/Pamud/conn.php
@@ -0,0 +1,10 @@
+<?php
+$db_name="ereader";
+$mysql_username="root";
+$mysql_password="";
+$sever_name="localhost";
+$conn=mysqli_connect($sever_name,$mysql_username,$mysql_password,$db_name);
+
+
+
+?>
